cb749ff32b2a41918370ac9b1b067ca77a9d1a66
LinhNV <vietlinhtspt@gmail.com>
2018-04-29 01:16:38 +0700

    Ve xong ma tran 10x10

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <SDL.h>
 #include <string>
+#include <ctime>
 using namespace std;
 void logSDLError(std::ostream& os, const std::string &msg, bool fatal = false);
 
@@ -25,12 +26,22 @@ void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, int x, int y);
 // Ảnh sẽ được co dãn (phóng to thu nhỏ) cho khớp kích cỡ này.
 // Đây là hàm không có trong tutorial ban đầu, được sửa từ hàm trong tutorial.
 void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, int x, int y, int w, int h);
-
-using namespace std;
+//ham chay game
+void playGame(SDL_Renderer* renderer);
+//Ham ve nen game
+void displayBackground(SDL_Renderer* renderer);
+//Ham ve ban co tro choi
+void displayMatrixColor(SDL_Renderer* renderer, int color[10][10]);
+//Ham ve hinh vuong 4 o
+void hinhVuong4(SDL_Renderer* renderer, int dentaX, int dentaY, int x, int y);
+//Ham ve thanh 4 o doc
+void doc4(SDL_Renderer* renderer, int dentaX, int dentaY, int x, int y);
+//Ham ve thanh 4 o ngang
+void ngang4(SDL_Renderer* renderer, int dentaX, int dentaY, int x, int y);
 
 //**************************************************************
-const int SCREEN_WIDTH = 450;
-const int SCREEN_HEIGHT = 800;
+const int SCREEN_WIDTH = 360;
+const int SCREEN_HEIGHT = 640;
 const char WINDOW_TITLE[] = "Hello";
 
 int main(int argc, char* argv[])
@@ -52,8 +63,8 @@ int main(int argc, char* argv[])
 		}
 	}
 	//Kich thuoc anh
-	int iSizeX = 450;
-	int iSizeY = 800;
+	int iSizeX = 360;
+	int iSizeY = 640;
 	//Vi tri
 	int x = 0;
 	int y = 0;
@@ -71,18 +82,32 @@ int main(int argc, char* argv[])
 		SDL_RenderPresent(renderer);
 		while (SDL_WaitEvent(&mainEven)) {
 			// Nếu sự kiện là kết thúc (như đóng cửa sổ) thì thoát khỏi vòng lặp
-			switch (SDL_WaitEvent(&mainEven)) {
-			case SDL_QUIT: {
-				isRunning = false;
-				break;
-			}
-			case SDL_MOUSEMOTION: {
-				cout << "Current Position Mouse: (" << mainEven.motion.x << "," << mainEven.motion.y << ")\n";
-				break;
-			}
-			default:
-				cout << "default";
-				break;
+			switch (mainEven.type) {
+				//Truong hop thoat
+				case SDL_QUIT: {
+					isRunning = false;
+					break;
+				}
+				case SDL_MOUSEBUTTONUP:
+				{
+					if (mainEven.button.button == SDL_BUTTON_LEFT)
+					{
+						if ((mainEven.motion.x >= 60) && (mainEven.motion.x <= 300) && (mainEven.motion.y >= 220) && (mainEven.motion.y <= 325)) {
+							playGame(renderer);
+						}
+					}
+					break;
+				}
+				case SDL_MOUSEMOTION: {
+					cout << mainEven.motion.x << " , " << mainEven.motion.y << "\n";
+					break;
+				}
+				//Truong hop khac
+				default: {
+					cout << "default";
+					break;
+				}
+					
 			}
 		}
 	}
@@ -93,9 +118,6 @@ int main(int argc, char* argv[])
 	//Ham giai phong bo nho
 	quitSDL(window, renderer);
 	return 0;
-
-	system("pause");
-	return 1;
 }
 //*****************************************************
 // Các hàm chung về khởi tạo và huỷ SDL
@@ -216,4 +238,198 @@ void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, int x, int y, int w, int
 	//(ảnh sẽ co dãn cho khớp với kích cỡ mới)
 	SDL_RenderCopy(ren, tex, NULL, &dst);
 }
+
+void playGame(SDL_Renderer* renderer)
+{
+	//Matrix luu trang thai cac o 
+	int color[10][10];
+	//Duu trang thai cac o ve trong
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 10; j++) {
+			color[i][j] = 0;
+		}
+	}
+	//Hien thi nen game
+	displayBackground(renderer);
+	displayMatrixColor(renderer, color);
+	//Su Kien
+	SDL_Event playEvent;
+	//Chay game
+	bool isRunning = true;
+	while (isRunning) {
+		/*srand(time(NULL));
+		int a = rand() % 50 + 1;*/
+		while (SDL_WaitEvent(&playEvent)) {
+			// Nếu sự kiện là kết thúc (như đóng cửa sổ) thì thoát khỏi vòng lặp
+			switch (playEvent.type) {
+				//Truong hop thoat
+				case SDL_QUIT: {
+					isRunning = false;
+					break;
+				}
+				case SDL_MOUSEMOTION: {
+					cout << playEvent.motion.x << " , " << playEvent.motion.y << "\n";
+					break;
+				}
+				case SDL_MOUSEBUTTONUP:
+				{
+					if (playEvent.button.button == SDL_BUTTON_LEFT)
+					{
+						if ((playEvent.motion.x >= 35) && (playEvent.motion.x <= 95) && (playEvent.motion.y >= 530) && (playEvent.motion.y <= 590)) {
+						}
+					}
+					break;
+				}
+				
+			}
+		}
+	}
+}
+void displayBackground(SDL_Renderer* renderer) {
+	//Load anh khi vao game
+	SDL_Texture* picture;
+	picture = loadTexture("picture/play.bmp", renderer);
+	//Kich thuoc anh
+	int iSizeX = 360;
+	int iSizeY = 640;
+	//Vi tri
+	int x = 0;
+	int y = 0;
+	//Ve anh
+	renderTexture(picture, renderer, x, y, iSizeX, iSizeY);
+	SDL_RenderPresent(renderer);
+}
+void displayMatrixColor(SDL_Renderer* renderer, int color[10][10]) {
+	//Kich thuoc anh
+	int iSizeX = 29;
+	int iSizeY = 29;
+	//Vi tri
+	int x = 30;
+	int y = 160;
+	//Vong lap cac o vuong thanh hinh
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 10; j++) {
+			//Load Picture
+			SDL_Texture* picture;
+			//Select the color accordingly
+			switch (color[i][j]) {
+				//Original cell
+			case 0: {
+				picture = loadTexture("picture/00.bmp", renderer);
+				//Display 
+				renderTexture(picture, renderer, x + j * 30, y + i * 30, iSizeX, iSizeY);
+				SDL_RenderPresent(renderer);
+				break;
+			}
+					//Blue
+			case 1: {
+				picture = loadTexture("picture/01_Blue.bmp", renderer);
+				//Display 
+				renderTexture(picture, renderer, x + j * 30, y + i * 30, iSizeX, iSizeY);
+				SDL_RenderPresent(renderer);
+				break;
+			}
+					//Emerald Green
+			case 2: {
+				picture = loadTexture("picture/02_EmeraldGreen.bmp", renderer);
+				//Display 
+				renderTexture(picture, renderer, x + j * 30, y + i * 30, iSizeX, iSizeY);
+				SDL_RenderPresent(renderer);
+				break;
+			}
+					//Green
+			case 3: {
+				picture = loadTexture("picture/03_Green.bmp", renderer);
+				//Display 
+				renderTexture(picture, renderer, x + j * 30, y + i * 30, iSizeX, iSizeY);
+				SDL_RenderPresent(renderer);
+				break;
+			}
+					//Orange
+			case 4: {
+				picture = loadTexture("picture/04_Orange.bmp", renderer);
+				//Display 
+				renderTexture(picture, renderer, x + j * 30, y + i * 30, iSizeX, iSizeY);
+				SDL_RenderPresent(renderer);
+				break;
+			}
+					//Pink
+			case 5: {
+				picture = loadTexture("picture/05_Pink.bmp", renderer);
+				//Display 
+				renderTexture(picture, renderer, x + j * 30, y + i * 30, iSizeX, iSizeY);
+				SDL_RenderPresent(renderer);
+				break;
+			}
+					//Purple
+			case 6: {
+				picture = loadTexture("picture/06_Purple.bmp", renderer);
+				//Display 
+				renderTexture(picture, renderer, x + j * 30, y + i * 30, iSizeX, iSizeY);
+				SDL_RenderPresent(renderer);
+				break;
+			}
+					//Red
+			case 7: {
+				picture = loadTexture("picture/07_Red.bmp", renderer);
+				//Display 
+				renderTexture(picture, renderer, x + j * 30, y + i * 30, iSizeX, iSizeY);
+				SDL_RenderPresent(renderer);
+				break;
+			}
+					//Yellow
+			case 8: {
+				picture = loadTexture("picture/08_Yellow.bmp", renderer);
+				//Display 
+				renderTexture(picture, renderer, x + j * 30, y + i * 30, iSizeX, iSizeY);
+				SDL_RenderPresent(renderer);
+				break;
+			}
+			}
+		}
+	}
+}
+void hinhVuong4(SDL_Renderer* renderer, int dentaX, int dentaY, int x, int y)
+{
+	//Load anh
+	SDL_Texture* picture;
+	picture = loadTexture("picture/03_Green.bmp", renderer);
+	//Kich Thuoc anh
+	int iSizeX = 29;
+	int iSizeY = 29;
+	//vong lap ve hinh
+	for (int i = 0; i <= 1; i ++) {
+		for (int j = 0; j <= 1; j ++) {
+			renderTexture(picture, renderer, x + dentaX, y + dentaY, iSizeX + j * 30, iSizeY + i * 30);
+			SDL_RenderPresent(renderer);
+		}
+	}
+}
+void ngang4(SDL_Renderer* renderer, int dentaX, int dentaY, int x, int y) {
+	//Load anh
+	SDL_Texture* picture;
+	picture = loadTexture("07_picture/Red.bmp", renderer);
+	//Kich Thuoc anh
+	int iSizeX = 29;
+	int iSizeY = 29;
+	//vong lap ve hinh
+	for (int i = 0; i <= 3; i++) {
+		renderTexture(picture, renderer, x + dentaX, y + dentaY, iSizeX + i * 30, iSizeY);
+		SDL_RenderPresent(renderer);
+	}
+}
+void doc4(SDL_Renderer* renderer, int dentaX, int dentaY, int x, int y) {
+	//Load anh
+	SDL_Texture* picture;
+	picture = loadTexture("picture/04_Orange.bmp", renderer);
+	//Kich Thuoc anh
+	int iSizeX = 29;
+	int iSizeY = 29;
+	//vong lap ve hinh
+	for (int i = 0; i <= 3; i++) {
+		renderTexture(picture, renderer, x + dentaX, y + dentaY, iSizeX, iSizeY + i * 30);
+		SDL_RenderPresent(renderer);
+	}
+}
+
 
